917a082a6cd9575fd4a6bdbcd33d703d3bb6639a
U84506866-bit <u84506866@gmail.com>
2026-08-17 20:10:15 +0330

    restrucutrued and fixed more bugs

--- animals.py
+++ animals.py
@@ -1,45 +1,47 @@
 class Animal:
 
-    def __init__(self, name, age, gender, breed, adopted):
+    def __init__(self, name, age, gender, breed, id, adopted= False ):
 
         self.name = str(name)
         self.age = int(age)
         self.gender = str(gender)
         self.breed = str(breed)
         self.adopted = bool(adopted)
+        self.id = str(id)
+
 
     def show_info(self):
-        print(f"{self.name}:{self.age}{self.gender}:{self.breed}")
+        print(f"{self.name}:{self.age} {self.gender} {self.breed}")
 
 
 class Dog(Animal):
 
-    def __init__(self, name, age, gender, breed, adopted):
-        super().__init__(self, name, age, gender, breed, adopted=False)
+    def __init__(self, name, age, gender, breed, id,  adopted):
+        super().__init__(name, age, gender, breed, id, adopted)
 
     def  make_sound(self):
         print("Woof woof")
 
 class Cat(Animal):
 
-    def __init__(self, name, age, gender, breed):
-        super().__init__(self, name, age, gender, breed)
+    def __init__(self, name, age, gender, breed, id,  adopted):
+        super().__init__(name, age, gender, breed, id,  adopted)
 
     def make_sound(self):
         print("Meow meow")
 
 class Bird(Animal):
 
-    def __init__(self, name, age, gender, breed):
-        super().__init__(self,name,age,gender,breed)
+    def __init__(self, name, age, gender, breed, id,  adopted):
+        super().__init__(name,age,gender,breed, id, adopted)
 
     def make_sound(self):
         print("Screech screech")
 
 class Rabbit(Animal):
 
-    def __init__(self, name, age, gender, breed):
-        super().__init__(self,name,age,gender,breed)
+    def __init__(self, name, age, gender, breed, id,  adopted):
+        super().__init__(name,age,gender,breed , id, adopted)
 
     def make_sound(self):
         print("Squeak squeak")
