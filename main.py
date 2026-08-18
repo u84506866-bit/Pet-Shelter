3c2fcc0a34483403395c86cb084fab40c283f334
U84506866-bit <u84506866@gmail.com>
2026-08-18 13:06:51 +0330

    switch case added to main and ready to implement the functionality

--- main.py
+++ main.py
@@ -3,21 +3,83 @@ import people
 import shelter
 
 
-class main:
+class Main:
 
     def __init__(self):
 
-        self.main()
-
-    def main(self):
-
-        print("=========================\n Welcome to Animal Shelter CLI\n===========================")
-
-        animal_object = animals.Animal()
+        self. shelter = shelter
+        self.__animal_list = list()
+        self.__person_list = list()
         
+        self.main_menu()
 
 
+    def main_menu(self):
+
+        print("=======================================\n  🐾 Welcome to Animal Shelter CLI 🐾\n=======================================\n")
+        
+        print("What you want to do?\n1. Add Animal\n2. Show Animals\n3. Search Animal\n4. Adopting Animal\n5. Return Animal\n6. Show Available Animals\n7. Show Adopted Animals\n8. Exit ")
+
+        try:
+
+            choice = int(input("\nEnter Choice: "))
+
+            while choice not in range(1,9):
+                choice = int(input("⚠️ Invalid Option Please Try Again: "))
+
+        except ValueError:
+            
+            print("❌ Error! You Didn't Enter a Number!")
+            self.main_menu()
+
+        match choice:
+
+            case 1:
+                add_animal()
+
+            case 2:
+                show_animals()
+
+            case 3:
+                search_animal()
+
+            case 4:
+                adopt_animal()
+
+            case 5:
+                return_animal()
+
+            case 6:
+                show_available_animals()
+
+            case 7:
+                show_adopted_animals()
+
+            case 8:
+                exit()
+
+
+        def add_animal(self):
+            self.Shelter.append
+
+        def show_animals(self,  animals_list):
+            pass
+
+        def search_animal(self, name):
+            pass
+
+        def adopt_animal(self,name,id):
+            pass
+
+        def return_animal(self):
+            pass
+
+        def show_available_animals(self):
+            pass
+
+        def show_adopted_animals(self):
+            pass 
 
 if __name__ == "__main__":
 
-    main()
+    Main()
